8cb6c30ed9ad342a2239053c4ab91c54aaf435f6
Laura Billi <34201070+LauraBilli@users.noreply.github.com>
2021-11-03 21:10:34 -0300

    Productos (back)

--- tp2-server/index.js
+++ tp2-server/index.js
@@ -18,6 +18,8 @@ const Usuarios = require('./models/user')
 const rutaCategorias = '/categoria';
 const Categorias = require('./models/categories')
 
+const rutaProductos = '/producto';
+const Productos = require('./models/products')
 
 app.use(cors());
 
@@ -137,8 +139,6 @@ app.post(rutaCategorias, async function (req, res) {
 app.put(rutaCategorias + '/editar/:id', (req, res) => {
 
     try{
-        console.log('EDITO ');
-        console.log(req.body);
         Categorias.findByIdAndUpdate(req.params.id, req.body)
         .then((data) => {
             res.status(200);
@@ -196,5 +196,98 @@ app.get(rutaCategorias + '/:id', (req, res) => {
 })
 
 // -----------------------------------------  FIN CATEGORIAS ------------------------------------------------------
+
+
+
+// -----------------------------------------   PRODUCTOS ------------------------------------------------------
+
+app.post(rutaProductos, async function (req, res) {
+
+    try{
+        const existe = await Productos.exists({ name : req.body.name })
+        if(!existe){
+            Productos.create(req.body)
+            .then((data) => {
+                res.status(200);
+                res.send(data);
+            })
+            .catch(err => {
+                console.log(err.message);
+                res.status(404).end(); 
+            })
+        }else{
+            console.log("El producto ya existe");
+            res.status(409);
+            res.send();
+        }
+    }catch(error){
+        console.log(error.message);
+        res.status(404).end(); 
+    }
+    
+})
+
+app.put(rutaProductos + '/editar/:id', (req, res) => {
+
+    try{
+
+        console.log(req.body);
+        Productos.findByIdAndUpdate(req.params.id, req.body)
+        .then((data) => {
+            res.status(200);
+            res.send(data);
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(404).end(); 
+        })
+        
+    }catch(error){
+        console.log(error.message);
+        res.status(404).end(); 
+    }
+    
+})
+
+app.delete(rutaProductos + '/borrar/:id', (req, res) => {
+
+    Productos.findByIdAndDelete({ _id : req.params.id }) 
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        console.log(err.message);
+        res.status(404).end(); 
+    })
+
+})
+
+app.get(rutaProductos, (req, res) => {
+
+    Productos.find()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        console.log(err.message);
+        res.status(404).end(); 
+    })
+
+})
+
+app.get(rutaProductos + '/:id', (req, res) => {
+
+    Productos.findById({ _id : req.params.id }) 
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        console.log(err.message);
+        res.status(404).end(); 
+    })
+
+})
+
+// -----------------------------------------  FIN PRODUCTOS ------------------------------------------------------
 
 app.listen(puerto); 
---
+++ tp2-server/models/products.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+//IMPORTANTE: Los campos que se manden desde afuera, que no estén en el modelo, los ignora (por eso no filtraba)
+
+const Product = new Schema({
+    name : {
+        type: String,
+        required: true
+    }
+}, {
+    collection: 'products'
+})
+
+module.exports = mongoose.model('Product', Product)
